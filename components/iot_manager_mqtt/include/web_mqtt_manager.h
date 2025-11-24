df7fa7227a501c57e61acc3d5ca2558cfaed2cc4
xingnian <jixingnian@gmail.com>
2025-11-24 12:25:05 +0800

    创建项目

---
+++ components/iot_manager_mqtt/include/web_mqtt_manager.h
@@ -0,0 +1,13 @@
+/*
+ * @Author: 星年 && jixingnian@gmail.com
+ * @Date: 2025-11-24 12:17:40
+ * @LastEditors: xingnian jixingnian@gmail.com
+ * @LastEditTime: 2025-11-24 12:23:15
+ * @FilePath: \xn_web_mqtt_manager\components\iot_manager_mqtt\include\web_mqtt_manager.h
+ * @Description: Web MQTT 管理器
+ * 
+ * Copyright (c) 2025 by ${git_name_email}, All Rights Reserved. 
+ */
+
+
+
